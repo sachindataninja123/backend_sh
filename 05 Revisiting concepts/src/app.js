21783f45ca7956e15d642d0ec9d19e48ac6440f5
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-03-08 23:28:31 +0530

    folder initialization

---
+++ 05 Revisiting concepts/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path")
+
+const app = express();
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = app;
