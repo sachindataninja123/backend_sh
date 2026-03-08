5387b8da1225490d3773c2c48267c3299089cd93
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-03-08 18:21:51 +0530

    ejs setup

--- 04 Practice Concepts/src/app.js
+++ 04 Practice Concepts/src/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
+});
+
+app.get("/profile/:username", (req, res) => {
+  const username = req.params.username;
+  console.log(username);
+
+  res.send(`welcome , ${req.params.username}`);
+});
+
+app.get("/profile/:username/:age", (req, res) => {
+  res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
 });
 
 module.exports = app;
