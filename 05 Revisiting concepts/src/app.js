d7cf51a1b815eac17df7fc5bf94131a7ec8d3137
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-03-09 20:50:13 +0530

    write file

--- 05 Revisiting concepts/src/app.js
+++ 05 Revisiting concepts/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
@@ -9,7 +10,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const filesPath = path.join(__dirname, "..", "files");
+  fs.readdir(filesPath, (err, files) => {
+    if (err) {
+      console.error("Error reading files:", err);
+      return res.status(500).send("Unable to read files");
+    }
+    // console.log(files);
+    res.render("index", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  const fileName = req.body.title.split(" ").join("") + ".txt";
+  const filesPath = path.join(__dirname, "..", "files", fileName);
+
+  fs.writeFile(filesPath, req.body.details, (err) => {
+    if (err) {
+      console.log("Error creating file:", err);
+      return res.status(500).send("Unable to create file");
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = app;
