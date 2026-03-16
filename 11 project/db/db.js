9b5686430156062027fc6a720c3aa210229a8d27
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-03-16 23:17:40 +0530

    Project

---
+++ 11 project/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connectDB =async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("Server is connected Successfully")
+    } catch (error) {
+        console.log("Server connection failed")
+    }
+}
+
+module.exports = connectDB
