06c616e89451ea9089250d8e9548ba97899836ae
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-04-14 23:37:08 +0530

    Project update

---
+++ 12 Bag Ecom project/controllers/ownerController.js
@@ -0,0 +1,46 @@
+const ownerModel = require("../models/owners.model");
+
+const createOwner = async (req, res) => {
+  try {
+    let { email, fullName, password } = req.body;
+
+    if (!email || !fullName || !password) {
+      return res.status(400).json({
+        success: false,
+        error: true,
+        message: "All fields are required",
+      });
+    }
+
+    let existingOwners = await ownerModel.findOne();
+
+    if (existingOwners) {
+      return res.status(403).json({
+        success: false,
+        error: true,
+        message: "You don't have permission to create a new owner",
+      });
+    }
+
+    const user = await ownerModel.create({
+      fullName,
+      email,
+      password,
+    });
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      user: user,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Server error",
+    });
+  }
+};
+
+module.exports = { createOwner };
--- 12 Bag Ecom project/routes/ownerRoutes.js
+++ 12 Bag Ecom project/routes/ownerRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { createOwner } = require("../controllers/ownerController");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("owner");
 });
+
+router.post("/create", createOwner);
 
 module.exports = router;
